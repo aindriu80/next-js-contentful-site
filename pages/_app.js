bfc165ca304578efc3288b94f1534c1ef11d3897
aindriu80 <aindriu80@gmail.com>
2021-05-09 14:56:51 +0100

    Outputting the Recipe data

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Just add Marmite</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
