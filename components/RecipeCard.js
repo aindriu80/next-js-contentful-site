7779de8c42c78a929ba0e2909bca7a80c8f22f1c
aindriu80 <aindriu80@gmail.com>
2021-05-10 15:56:00 +0100

    Displaying images from Contentful via Next.js

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,10 +1,17 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function RecipeCard({ recipe }) {
   const { title, slug, cookingTime, thumbnail } = recipe.fields
   return (
     <div className="card">
-      <div className="featured">{/* Image - thumbnail */}</div>
+      <div className="featured">
+        <Image
+          src={'https:' + thumbnail.fields.file.url}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+        />
+      </div>
       <div className="contents">
         <div className="info">
           <h4>{title}</h4>
